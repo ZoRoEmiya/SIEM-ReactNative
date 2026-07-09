13f5cf6c433ddf81222fbd33a3b4872a1a2c589a
YuvalThePro <ifraimovy22@gmail.com>
2026-07-10 02:59:13 +0300

    feat: moved completly to react native

--- SIEMMobile/app/_layout.jsx
+++ SIEMMobile/app/_layout.jsx
@@ -10,7 +10,6 @@ export default function RootLayout() {
         <StatusBar style="dark" />
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="index" />
-          <Stack.Screen name="(auth)" />
           <Stack.Screen name="(tabs)" />
         </Stack>
       </AuthProvider>
